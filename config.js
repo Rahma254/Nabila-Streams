f0e70e5d2268cd42417b8e2fe54f09ee2951d098
Rahma254 <rahmasiska661@gmail.com>
2025-07-01 21:55:18 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+export const SUPABASE_URL = "https://vgchzuqtrmohyzojvngw.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnY2h6dXF0cm1vaHl6b2p2bmd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNzE1NzAsImV4cCI6MjA2Njg0NzU3MH0.WKcSXto5EXcS1fdScAKf6atW7tcXM9AB9jObapii_2g";
